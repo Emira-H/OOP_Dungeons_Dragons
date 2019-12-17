e1e4992e0f62d1b40ee8ae078390a9f2b05d7567
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-12-17 17:47:57 +0100

    bigining to implenments class charackter

--- character/character.py
+++ character/character.py
@@ -1,14 +1,26 @@
 class Character:
-    
+
     def __init__(self,name,life,attack,agility,defense):
         self.name=name
         self._attack=attack
         self.agility=agility
         self.defense=defense
-    
-    @property
-    def attack(self):
-        return self._attack
-    
 
-        
+
+    def skip(self):
+        """je passe au tour suivant et renvoi zero"""
+        return 0
+
+    def cheek_attack(self):
+        """je verifier en fonction de l'agilité s'il doit attaqé
+        ou non"""
+        import random
+        up_to_100=random.randint(1,100)
+        if(up_to_100 in range(1,101-self.agility)):
+            return self._attack
+        else:
+            return 0
+
+
+
+
