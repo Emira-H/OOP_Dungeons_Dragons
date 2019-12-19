6d5a076b7fd4d60d743c3351b68cce3f01595d67
kassama fally <fally.kassama@outlook.com>
2019-12-19 12:06:51 +0100

    arena conception

--- Game/arena.py
+++ Game/arena.py
@@ -1,13 +1,43 @@
-from character import Character
+from factory import Factory
 
 
-class Arena(Character):
+class Arena:
 
-    def __init__(self,):
-        super().__init__()
+    def __init__(self,combatant_1,combatant_2):
+        self.combatant_1 = self.player_choose
+        self.combatant_2 = self.pc_choose
 
 
-    def dommage(self,):
+
+    def attack_or_escape(self,attack):
+        if attack == "o":
+            self.attack_or_escape == True
+            return self.attack_or_escape
+        else :
+            self.attack_or_escape == False
+
+
+
+    def attack_adverse(self,attack_adverse):
+        hero = self.combatant_1
+        ennemie = self.combatant_2
+
+        self.attack_level = hero.character.attack
+
+
+    def combat(self,hero,ennemie):
+        combatant = None
+        hero = self.combatant_1
+        ennemie = self.combatant_2
+
+        if self.attack_or_escape == True
+        combatant = self.combatant_1
+        return combatant
+        
+
+
+    def dommage(self):
+        combatant = None
 
         if defense <= 0:
             life -= attack_adverse
@@ -18,5 +48,7 @@ class Arena(Character):
         else :
             life -=(attack_adverse-defense)
             defense = 0
-    def
 
+    def dead(self,combatant_1,combatant_2)
+
+    def
